e47082f8fa11bf06e81f537229b6acbbfc092c05
Michael Romay <amerror@gmail.com>
2020-10-01 17:13:56 -0700

    added some comments in fetch service call

--- src/services/fundingReliefAPI.js
+++ src/services/fundingReliefAPI.js
@@ -10,4 +10,6 @@ export const fetchDetail = (id) => {
   return fetch(`${process.env.API_URL}/campaigns/${id}`)
     .then((res) => res.json())
     .then((json) => json);
+  //originally this:
+  //.then((json) => json[0]);
 };
