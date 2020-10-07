d5562aad884dd88c1690873120beb55811b3f7f6
Michael Romay <amerror@gmail.com>
2020-10-06 18:06:33 -0700

    created switch links for all the different campaign locations

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,6 +4,10 @@ import Header from '../Header/Header';
 import CampaignList from '../../container/CampaignList/CampaignList';
 import CampaignDetail from '../../container/CampaignDetail/CampaignDetail';
 import OregonCampaignList from '../../container/CampaignList/OregonCampaignList';
+import NoCalCampaignList from '../../container/CampaignList/NoCalCampaignList';
+import SoCalCampaignList from '../../container/CampaignList/SoCalCampaignList';
+import CentralCalCampaignList from '../../container/CampaignList/CentralCalCampaignList';
+import WashingtonCampaignList from '../../container/CampaignList/WashingtonCampaignList';
 import About from '../About/About';
 
 export default function App() {
@@ -16,6 +20,26 @@ export default function App() {
           exact
           path="/campaigns/oregon-fires"
           component={OregonCampaignList}
+        />
+        <Route
+          exact
+          path="/campaigns/no-cal-fires"
+          component={NoCalCampaignList}
+        />
+        <Route
+          exact
+          path="/campaigns/so-cal-fires"
+          component={SoCalCampaignList}
+        />
+        <Route
+          exact
+          path="/campaigns/central-cal-fires"
+          component={CentralCalCampaignList}
+        />
+        <Route
+          exact
+          path="/campaigns/washington-fires"
+          component={WashingtonCampaignList}
         />
         <Route exact path="/campaigns/:id?" component={CampaignDetail} />
         <Route exact path="/about" component={About} />
