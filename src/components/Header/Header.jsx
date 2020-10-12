ac0e2d821e66350f8e6f50b76deffcdbddb439a3
Will G Piro <willgpiro@gmail.com>
2020-10-12 15:27:38 -0700

    Add Semantic UI, Create Header
    Modified webpack.config 75-94. Added scripts to index.html in head and body for semanticUI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,24 +1,63 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import styles from './Header.css';
+import { Input, Menu, Segment, Icon, Dropdown, Container} from 'semantic-ui-react';
 
 const Header = () => {
+
   return (
     <>
-      <header className={styles.Header}>
-        <h1 className={styles.logo}>
-          <Link to={'/'}>funding relief</Link>
-        </h1>
-        <nav className={styles.navbar}>
-          <h4>
-            <Link className={styles.navLink} to={'/about'}>
-              about
-            </Link>
-          </h4>
-        </nav>
-      </header>
+      <Segment
+        textAlign='center'
+        style={{ minHeight: '1em', padding: '1em 0em' }}
+        vertical
+        id={styles.segment}>
+        <Menu secondary inverted>
+          <Container className={styles.container}>
+          <Dropdown 
+            text='Campaigns' 
+            pointing
+            className='link item' 
+            floating>
+              <Dropdown.Menu >
+                <Dropdown.Header>Disasters</Dropdown.Header>
+                <Dropdown.Item>
+                  <Dropdown text='Wildfire'>
+                    <Dropdown.Menu>
+                      <Dropdown.Header>California</Dropdown.Header>
+                      <Dropdown.Item href={"/campaigns/no-cal-fires"}>
+                        Nor-Cal Fires
+                      </Dropdown.Item>
+                      <Dropdown.Item href={"/campaigns/central-cal-fires"}>
+                        Central-Cal Fires
+                      </Dropdown.Item>
+                      <Dropdown.Item href={"/campaigns/so-cal-fires"}>
+                        So-Cal Fires
+                      </Dropdown.Item>
+                      <Dropdown.Divider />
+                      <Dropdown.Header>Oregon</Dropdown.Header>
+                      <Dropdown.Item href={"/campaigns/oregon-fires"}>
+                        Oregon Fires (All)
+                      </Dropdown.Item>
+                      <Dropdown.Divider />
+                      <Dropdown.Header>Washington</Dropdown.Header>
+                      <Dropdown.Item href={"/campaigns/washington-fires"}>
+                        Washington Fires (All)
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+                <Menu.Item position="right"
+                  name='Funding Relief'
+                  href={"/"}
+              />
+          </Container>
+        </Menu>
+      </Segment>
     </>
   );
-};
+}
 
 export default Header;
+ 
