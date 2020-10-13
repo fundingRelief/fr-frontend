d5a97fbfe9b06cb125059659f7fbe34ba88f5b9e
Jaime Sanders <jaimelyn517@gmail.com>
2020-10-12 18:33:22 -0700

    adds sub items under About

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
-import { Menu, Input, Segment } from 'semantic-ui-react';
+// import styles from './Footer.css';
+import { Segment } from 'semantic-ui-react';
+
 
 const Footer = () => {
 
   return (
-  // <Menu>
-  //   <Menu.Item disabled>
-  //     (c) My Copyright
-  //   </Menu.Item>
-  //   <Menu.Item position="right">
-  //     <Input disabled label="Autor:"/>
-  //   </Menu.Item>
-  //   <Menu.Item >
-  //     <Input disabled label="Date:"/>
-  //   </Menu.Item>
-  // </Menu>
-
     <Segment inverted vertical as="footer">
       <div className="ui inverted vertical footer segment">
         <div className="ui container">
           <div className="ui stackable inverted divided equal height stackable grid">
             <div className="three wide colum">
               <h4 className="ui inverted header">About</h4>
-
+              <div className="ui inverted link list">
+                <a href="#" className="item">
+                    The Site
+                </a>
+                <a href="#" className="item">
+                    The Developers
+                </a>
+                <a href="#" className="item">
+                    Contact Us
+                </a>
+              </div>
             </div>
 
           </div>
