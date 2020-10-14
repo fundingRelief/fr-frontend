7a6b76c903131bce3a4e84528c7f65af9bff0e73
Michael Romay <amerror@gmail.com>
2020-10-13 20:48:18 -0700

    cleaned up CampaingList

--- src/container/CampaignDetail/CampaignDetail.jsx
+++ src/container/CampaignDetail/CampaignDetail.jsx
@@ -13,8 +13,6 @@ const CampaignDetail = () => {
     );
   }, []);
 
-  //I did this map because the page was erroring out because it doesn't await the data.  isRequired from PropTypes wasn't working correctly and the initial fetch was json[0].  The numberWithCommas function wouldn't work either because there was no data in it.  If I do this map then everything works the way it should, although there is likely a better way to accomplish this, please feel free to change.
-
   const campaignNodes = campaign.map((detail) => {
     return <Details key={detail.id} {...detail} />;
   });
--- src/container/CampaignList/CampaignList.jsx
+++ src/container/CampaignList/CampaignList.jsx
@@ -11,24 +11,22 @@ const CampaignList = () => {
   }, []);
 
   const campaignNodes = campaigns.map((campaign) => {
-    return <List key={campaign.id} {...campaign} />
-  
+    return <List key={campaign.id} {...campaign} />;
   });
 
   return (
     <>
-    <Container style={{ padding: '5em' }}>
-      <Segment>
-        <Card.Group 
-          itemsPerRow={3}
-          stackable="true">
+      <Container style={{ padding: '5em' }}>
+        <Segment>
+          <Card.Group 
+            itemsPerRow={3}
+            stackable="true">
             {campaignNodes}
-        </Card.Group>
-      </Segment>
-    </Container>
+          </Card.Group>
+        </Segment>
+      </Container>
     </>
-    );
-  ;
+  );
 };
 
 export default CampaignList;
