0d81f6e4d1d78bdadc2c5270f958f6e0d884bf2b
Michael Romay <amerror@gmail.com>
2020-10-13 20:42:48 -0700

    fixed bottom link for donate now and set both links to open a new tab when clicked

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import styles from './List.css';
 import { Card, Icon, Image, Progress } from 'semantic-ui-react';
 
@@ -23,19 +23,21 @@ const List = ({
 
   return (
     <Card 
-      color='teal'
+      color="teal"
       raised={true}>
       <Card.Content>
         <Image 
-          bordered rounded size='large'
+          bordered rounded size="large"
           href={link_url}
-          style={{ backgroundImage: `url(${img_url})`}}
+          target="_blank" 
+          rel="noopener noreferrer"
+          style={{ backgroundImage: `url(${img_url})` }}
           className={styles.cardImg}
-          />
+        />
         <Card.Header 
           className={styles.header}
-          textAlign={"left"}
-          href={link_url}>        
+          textAlign={'left'}
+          href={link_url} target="_blank" rel="noopener noreferrer">        
           {campaign_name}
         </Card.Header>
         <Card.Description className={styles.location}>
@@ -52,19 +54,19 @@ const List = ({
             percent={percentage_raised} 
             progress
             className={styles.progressBar}
-            color='teal'/>
+            color="teal"/>
         </Card.Meta>
         <Card.Meta className={styles.raised}>
           <strong>${numberWithCommas(current_amount)} raised of </strong> ${numberWithCommas(goal)}{' '}goal
         </Card.Meta>
       </Card.Content>
-        <Card.Content 
-          extra
-          textAlign="center">
-          <a>
-            <Icon name='user' href={link_url} />
+      <Card.Content 
+        extra
+        textAlign="center">
+        <a href={link_url} target="_blank" rel="noopener noreferrer">
+          <Icon name="user" />
               Donate Now at GoFundMe
-          </a>
+        </a>
       </Card.Content>
     </Card>
   );
@@ -79,7 +81,7 @@ List.propTypes = {
   location: PropTypes.string.isRequired,
   img_url: PropTypes.string.isRequired,
   link_url: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  description: PropTypes.string,
   last_donation: PropTypes.string.isRequired,
 };
 
