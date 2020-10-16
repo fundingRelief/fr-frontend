1e45248adac850ec2c6a8bc276f8e2309ef9047d
Michael Romay <amerror@gmail.com>
2020-10-15 18:04:09 -0700

    created a conditional loading display for CampaignDetail

--- src/container/CampaignDetail/CampaignDetail.jsx
+++ src/container/CampaignDetail/CampaignDetail.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Details from '../../components/Details/Details';
-import { fetchDetail } from '../../services/fundingReliefAPI';
+import { fetchCampaignById } from '../../services/fundingReliefAPI';
+// import { fetchDetail } from '../../services/fundingReliefAPI';
 
 const CampaignDetail = () => {
   const [campaign, setCampaign] = useState([]);
   const { id } = useParams();
 
+  // useEffect(() => {
+  //   fetchDetail(id).then((fetchCampaignDetail) =>
+  //     setCampaign(fetchCampaignDetail)
+  //   );
+  // }, []);
+
   useEffect(() => {
-    fetchDetail(id).then((fetchCampaignDetail) =>
-      setCampaign(fetchCampaignDetail)
-    );
+    fetchCampaignById(id)
+      .then(campaign => {
+        setCampaign(campaign);
+      });
   }, []);
 
   const campaignNodes = campaign.map((detail) => {
     return <Details key={detail.id} {...detail} />;
   });
 
+  if(!campaign)
+    return <h1>loading</h1>;
+    
   return <>{campaignNodes}</>;
 };
 
