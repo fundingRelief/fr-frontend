6a59979202ffd3c4dc000237d1ac11deeb250ec2
Michael Romay <amerror@gmail.com>
2020-10-15 18:11:19 -0700

    cleaned up fuindgReliefAPI js

--- src/services/fundingReliefAPI.js
+++ src/services/fundingReliefAPI.js
@@ -1,38 +1,10 @@
 import { get } from './request';
 
 export const fetchCampaigns = () => get('/campaigns');
-
 export const fetchCampaignById = campaignId => get(`/campaigns/${campaignId}`);
 
-/* eslint-disable no-undef */
-
-
-export const fetchListOregon = () => {
-  return fetch(`${process.env.API_URL}/campaigns/oregon`)
-    .then((res) => res.json())
-    .then((json) => json);
-};
-
-export const fetchListNoCal = () => {
-  return fetch(`${process.env.API_URL}/campaigns/nocal`)
-    .then((res) => res.json())
-    .then((json) => json);
-};
-
-export const fetchListSoCal = () => {
-  return fetch(`${process.env.API_URL}/campaigns/socal`)
-    .then((res) => res.json())
-    .then((json) => json);
-};
-
-export const fetchListCentralCal = () => {
-  return fetch(`${process.env.API_URL}/campaigns/centralcal`)
-    .then((res) => res.json())
-    .then((json) => json);
-};
-
-export const fetchListWashington = () => {
-  return fetch(`${process.env.API_URL}/campaigns/washington`)
-    .then((res) => res.json())
-    .then((json) => json);
-};
+export const fetchListOregon = () => get('/campaigns/oregon');
+export const fetchListNoCal = () => get('/campaigns/nocal');
+export const fetchListSoCal = () => get('/campaigns/socal');
+export const fetchListCentralCal = () => get('/campaigns/centralcal');
+export const fetchListWashington = () => get('/campaigns/washington');
