60a8fd80b9397c48c10cb236758843be9c0e88e0
Michael Romay <amerror@gmail.com>
2020-10-15 18:08:29 -0700

    cleaning up fundingReliefAPI js

--- src/services/fundingReliefAPI.js
+++ src/services/fundingReliefAPI.js
@@ -4,26 +4,8 @@ export const fetchCampaigns = () => get('/campaigns');
 
 export const fetchCampaignById = campaignId => get(`/campaigns/${campaignId}`);
 
-
-
-
-
 /* eslint-disable no-undef */
 
-
-
-
-// export const fetchFunction = () => {
-//   return fetch(`${process.env.API_URL}/campaigns`)
-//     .then((res) => res.json())
-//     .then((json) => json);
-// };
-
-// export const fetchList = () => {
-//   return fetch(`${process.env.API_URL}/campaigns/percentage`)
-//     .then((res) => res.json())
-//     .then((json) => json);
-// };
 
 export const fetchListOregon = () => {
   return fetch(`${process.env.API_URL}/campaigns/oregon`)
@@ -53,12 +35,4 @@ export const fetchListWashington = () => {
   return fetch(`${process.env.API_URL}/campaigns/washington`)
     .then((res) => res.json())
     .then((json) => json);
-};
-
-export const fetchDetail = (id) => {
-  return fetch(`${process.env.API_URL}/campaigns/${id}`)
-    .then((res) => res.json())
-    .then((json) => json);
-  //originally this:
-  //.then((json) => json[0]);
 };
