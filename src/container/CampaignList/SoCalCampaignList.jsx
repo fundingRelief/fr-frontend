f01c344961e93da431f2ff327eca4ceed6ed2954
Michael Romay <amerror@gmail.com>
2020-10-15 18:39:50 -0700

    added loading state to SoCalCampaignList jsx

--- src/container/CampaignList/SoCalCampaignList.jsx
+++ src/container/CampaignList/SoCalCampaignList.jsx
@@ -5,7 +5,7 @@ import { fetchListSoCal } from '../../services/fundingReliefAPI';
 import { useGetChallenges } from '../../hooks/getCampaigns';
 
 const SoCalCampaignList = () => {
-  const { campaigns } = useGetChallenges(fetchListSoCal);
+  const { campaigns, loading } = useGetChallenges(fetchListSoCal);
 
   const campaignNodes = campaigns.map((campaign) => {
     return <List key={campaign.id} {...campaign} />;
@@ -15,6 +15,7 @@ const SoCalCampaignList = () => {
     <>
       <Container style={{ padding: '5em' }}>
         <Segment>
+          {loading && <h3>loading...</h3>}
           <Card.Group 
             itemsPerRow={3}
             stackable={true}>
