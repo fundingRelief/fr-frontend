1e58474c7b091203d91bea12b202052c23962ad8
Michael Romay <amerror@gmail.com>
2020-10-15 18:20:57 -0700

    removed useEffect fetch / state from OregonCampaignList jsx

--- src/container/CampaignList/OregonCampaignList.jsx
+++ src/container/CampaignList/OregonCampaignList.jsx
@@ -1,16 +1,11 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import List from '../../components/List/List';
 import { Card, Container, Segment } from 'semantic-ui-react';
 import { fetchListOregon } from '../../services/fundingReliefAPI';
+import { useGetChallenges } from '../../hooks/getCampaigns';
 
 const OregonCampaignList = () => {
-  const [campaigns, setCampaigns] = useState([]);
-
-  useEffect(() => {
-    fetchListOregon().then((fetchOregonCampaignList) =>
-      setCampaigns(fetchOregonCampaignList)
-    );
-  }, []);
+  const { campaigns } = useGetChallenges(fetchListOregon);
 
   const campaignNodes = campaigns.map((campaign) => {
     return <List key={campaign.id} {...campaign} />;
