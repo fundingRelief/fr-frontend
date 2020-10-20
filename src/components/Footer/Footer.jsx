3427f8c8df5d98378139779f17f7f05f9919fa4c
Jaime Sanders <jaimelyn517@gmail.com>
2020-10-20 12:25:45 -0700

    adds CSS for footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
       <div className="ui inverted vertical footer segment">
         <div className="ui container">
           <div className="ui stackable inverted divided equal height stackable grid">
-            <div className="three wide colum">
+            <div className="one wide colum">
               <h4 className="ui inverted header">About</h4>
               <div className="ui inverted link list">
                 <a href={'/about'} className="item">
